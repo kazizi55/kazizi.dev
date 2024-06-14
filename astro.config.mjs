de7974b9c0cf15456cf22f3fa1bed8d3b70ae20d
kazizi55 <kaz.redhot1201@gmail.com>
2024-06-14 10:11:24 +0900

    chore: enable web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
-
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-theme-cody.netlify.app',
+  site: 'https://www.kazizi.dev',
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
@@ -20,6 +20,10 @@ export default defineConfig({
       },
     }
   },
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
 
 
